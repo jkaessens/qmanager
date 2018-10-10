fcc1348db9120d6090c5bd9fbeb8c1ae31f77205
Jan Christian Kaessens <j.kaessens@ikmb.uni-kiel.de>
2018-10-10 15:16:48 +0200

    make tls detection more robust

--- src/protocol.rs
+++ src/protocol.rs
@@ -44,11 +44,18 @@ pub fn read_and_decode(source: &mut Stream) -> Result<String> {
     let tls_ver_major = (len >> 8) & 0xFF;
     let tls_ver_minor = (len >> 16) & 0xFF;
 
-    if tls_handshake == 22 && tls_ver_major == 3 && tls_ver_minor > 0 && tls_ver_minor < 4 {
+    source.read_exact(&mut buf[0..1])?;
+
+    if tls_handshake == 22
+        && tls_ver_major == 3
+        && tls_ver_minor > 0
+        && buf[0] != 0x7B
+        && buf[0] != 0x22
+    {
         // We got a TLS handshake on an insecure socket
         Err(::std::io::Error::from(::std::io::ErrorKind::InvalidData))
     } else {
-        source.read_exact(&mut buf)?;
+        source.read_exact(&mut buf[1..len as usize])?;
         Ok(String::from_utf8(buf).unwrap())
     }
 }
